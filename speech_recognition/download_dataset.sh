7c544b8a8e9736ff262bbddb135dda6352cdfa9a
victor <vili@nvidia.com>
2018-11-07 13:47:04 +0800

    add wget

--- speech_recognition/download_dataset.sh
+++ speech_recognition/download_dataset.sh
@@ -1,4 +1,4 @@
 # Script to download Librispeech Dataset
 #for script testing, only fetch minimal, clean dataset
-
+sudo pip install wget
 python data/librispeech.py #--files_to_use train-clean-100.tar.gz,dev-clean.tar.gz,test-clean.tar.gz
